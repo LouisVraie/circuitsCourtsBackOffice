8ddac6106e16ff5c85dfd75d956d5daa2605ce02
LouisVraie <louis.vraie@gmail.com>
2022-04-06 16:22:28 +0200

    modif afficherTableGestionProduitsProduits dans gestionProduits.cpp

--- gestionProduits.cpp
+++ gestionProduits.cpp
@@ -38,14 +38,14 @@ void MainWindow::afficherTableGestionProduitsRayons()
         int row = ui->tableWidget_gestionProduitsRayons->rowCount();
         ui->tableWidget_gestionProduitsRayons->setRowCount(row+1);
         //on crée une instance de QLabel qui va contenir l'image du rayon
-        QLabel *labelimage= new QLabel();
-        labelimage->setPixmap(QPixmap(resultSelectRayons.value("imageRayon").toString()).scaled(imageSize,imageSize));
-        labelimage->setScaledContents(true);
+        QLabel *labelImage= new QLabel();
+        labelImage->setPixmap(QPixmap(resultSelectRayons.value("imageRayon").toString()).scaled(imageSize,imageSize));
+        labelImage->setScaledContents(true);
         //inserer les valeur dans le tableau
         ui->tableWidget_gestionProduitsRayons->setCellWidget(row,0, new QCheckBox());
         ui->tableWidget_gestionProduitsRayons->setItem(row,1, new QTableWidgetItem(resultSelectRayons.value("numeroRayon").toString()));
         ui->tableWidget_gestionProduitsRayons->setItem(row,2, new QTableWidgetItem(resultSelectRayons.value("dateInscriptionRayon").toString()));
-        ui->tableWidget_gestionProduitsRayons->setCellWidget(row,3,labelimage);
+        ui->tableWidget_gestionProduitsRayons->setCellWidget(row,3,labelImage);
         ui->tableWidget_gestionProduitsRayons->setItem(row,4, new QTableWidgetItem(resultSelectRayons.value("libelleRayon").toString()));
     }
     ui->tableWidget_gestionProduitsRayons->resizeColumnsToContents();
@@ -72,14 +72,14 @@ void MainWindow::afficherTableGestionProduitsProduits()
         int row = ui->tableWidget_gestionProduitsProduits->rowCount();
         ui->tableWidget_gestionProduitsProduits->setRowCount(row+1);
         //on crée une instance de QLabel qui va contenir l'image du rayon
-        QLabel *labelimage= new QLabel();
-        labelimage->setPixmap(QPixmap(resultSelectProduits.value("imageProduit").toString()).scaled(imageSize,imageSize));
-        labelimage->setScaledContents(true);
+        QLabel *labelImage= new QLabel();
+        labelImage->setPixmap(QPixmap(resultSelectProduits.value("imageProduit").toString()).scaled(imageSize,imageSize));
+        labelImage->setScaledContents(true);
         //inserer les valeur dans le tableau
         ui->tableWidget_gestionProduitsProduits->setCellWidget(row,0, new QCheckBox());
         ui->tableWidget_gestionProduitsProduits->setItem(row,1, new QTableWidgetItem(resultSelectProduits.value("numeroProduit").toString()));
         ui->tableWidget_gestionProduitsProduits->setItem(row,2, new QTableWidgetItem(resultSelectProduits.value("dateInscriptionProduit").toString()));
-        ui->tableWidget_gestionProduitsProduits->setCellWidget(row,3,labelimage);
+        ui->tableWidget_gestionProduitsProduits->setCellWidget(row,3,labelImage);
         ui->tableWidget_gestionProduitsProduits->setItem(row,4, new QTableWidgetItem(resultSelectProduits.value("libelleRayon").toString()));
         ui->tableWidget_gestionProduitsProduits->setItem(row,5, new QTableWidgetItem(resultSelectProduits.value("libelleProduit").toString()));
     }
